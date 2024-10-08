3ddf2f37466884d1f674393c9ccec8d1b49ef7d4
Erwin Antepuesto <antepuesto.erwin@gmail.com>
2024-10-08 11:56:48 +0800

    samok schema

--- server.js
+++ server.js
@@ -1,41 +1,95 @@
 var express = require('express');
-var { graphql, buildSchema } = require("graphql");
+var { graphql, buildSchema, GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt } = require("graphql");
 var { createHandler } = require("graphql-http/lib/use/express");
 var { ruruHTML } = require("ruru/server")
  
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello: String
-    age: Int
-    randomFloat: Float!
-    isOver18: Boolean
-    fruits: [String!]!
-  }
-`)
+// var schema = buildSchema(`
+//   type Query {
+//     hello(name: String!):String
+//     age: Int
+//     randomFloat: Float!
+//     isOver18: Boolean
+//     fruits: [String!]!
+//     rollDice(numDice: Int!, numSides: Int): [Int]
+//     user: User
+//   }
+
+//   type User{
+//     id: Int
+//     name: String
+//   }
+// `)
+const User = new GraphQLObjectType({
+    name: 'User',
+    fields:{
+        id:{
+            type: GraphQLInt,
+        },
+        name: {
+            type: GraphQLString,
+        }
+    }
+})
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'Query',
+        fields:{
+            hello: {
+                type: GraphQLString,
+                resolve: ()=>{
+                    return "Hello World";
+                }
+            },
+
+            user: {
+                type: User,
+                resolve: () =>{
+                    return {
+                        id: 1,
+                        name: 'Erwin'
+                    }
+                }
+            }
+        }
+    })
+})
  
 // The rootValue provides a resolver function for each API endpoint
-var rootValue = {
-  hello() {
-    return "Hello world!"
-  },
-  age: () => {
-    return 25;
-  },
-  randomFloat(){
-    return Math.random()
-  },
-  isOver18: true,
-  fruits(){
-    return ['Apple', 'Banana', 'Pear'];
-  }
-}
+// var rootValue = {
+//   hello: ({name}) => {
+//     return "Hello " + name;
+//   },//with arguments
+//   age: () => {
+//     return 25;
+//   },
+//   randomFloat(){
+//     return Math.random()
+//   },
+//   isOver18: true,
+//   fruits(){
+//     return ['Apple', 'Banana', 'Pear'];
+//   },
+//   rollDice({ numDice, numSides }) {
+//     var output = []
+//     for (var i = 0; i < numDice; i++) {
+//       output.push(1 + Math.floor(Math.random() * (numSides || 6)))
+//     }
+//     return output
+//   },
+//   user: () => {
+//     return {
+//         id: 1,
+//         name: "Erwin"
+//     }
+//   }
+// }
  
 // Run the GraphQL query '{ hello }' and print out the response
 graphql({
   schema,
-  source: "{ age }",
-  rootValue
+  source: "{ age }"
 }).then(response => {
   console.log(response)
 })
@@ -44,8 +98,7 @@ const app = express();
 
 app.all('/graphql', 
     createHandler({ 
-        schema: schema, 
-        rootValue: rootValue
+        schema: schema
     })
 );
 
@@ -57,5 +110,5 @@ app.get("/", (_req, res) => {
 app.listen(4000);
 console.log(`
     Running a GraphQL API server at http://localhost:4000
-    Test: http://localhost:4000/graphql?query={hello,age} 
+    Test: http://localhost:4000/graphql
 `)
