6cfe3dd50f2327b5774e8876953c1edf236e1ccd
Erwin Antepuesto <antepuesto.erwin@gmail.com>
2024-10-10 16:07:57 +0800

    crud operations

--- src/graphql/models/user.js
+++ src/graphql/models/user.js
@@ -1,48 +1,73 @@
+import { ObjectId } from "mongodb";
+
 export const typeDef = /* GraphQL */ `
     type Query{
-        user: User
+        users: [User!]!
+        user(id: ID!): User
     }
 
     type Mutation {
-        createUser(user: newUserInput!): User
+        createUser(user: new_user!): User
+        deleteUser(id: ID!): Boolean
+        updateUser(id: ID!, update: update_user): User
     }
 
-    input newUserInput{
+    input new_user{
         name: String!
-        age: Int!
+        email: String!
+    }
+
+    input update_user{
+        name: String!
     }
 
     type User {
-        id: Int
+        id: ID!
         name: String
-        age: Int
+        email: String
     }
 `
 
 export const resolvers = {
     Query: {
-        user:()=>{
-            return{
-                id: 1,
-                name: 'Erwin'
-            }
+        users: async(_, args, { mongo }) => {
+            return mongo.users.find().limit(20).toArray();
+        },
+
+        user: async (obj, { id }, { mongo }) => {
+            return mongo.users.findOne( { _id: new ObjectId(id) });
         }
     },
 
     User: {
+        id: ({ id, _id }) => _id || id,
         name: (obj) => {
             return obj.name.toUpperCase()
         }
     },
 
     Mutation: {
-        createUser: async(_, { user }, context) => {
-            const movies = await context.mongo.movies.find().toArray()
-            console.log(movies)
+        createUser: async(_, { user }, { mongo }) => {
+            console.log(user);
+            
+            const res = await mongo.users.insertOne(user);
+            console.log(res);
+            console.log(user);
             return{
-                id: 1,
+                id: res.insertedId,
                 ...user,
             }
+        },
+        deleteUser: async(_, { id }, { mongo }) => {
+            await mongo.users.deleteOne({ _id: new ObjectId(id) })
+            return true;
+        },
+        updateUser: async(_, { id, update }, { mongo }) => {
+            const res = await mongo.users.updateOne(
+                { _id: new ObjectId(id) }, 
+                { $set: { name: update.name}}
+            )
+            return mongo.users.findOne({_id: new ObjectId(id)})
         }
     }
 }
